71faf39d8b264c36391b832f977d1690a5cbfe97
amir9979 <amir9979@gmail.com>
2019-06-26 20:52:00 +0300

    add

---
+++ features.py
@@ -0,0 +1,48 @@
+import os
+import json
+import networkx
+import csv
+import itertools
+
+
+call_graphs = r"C:\amirelm\component_importnace\data\d4j_lang\call_graphs"
+execution_graphs = r"C:\amirelm\component_importnace\data\d4j_lang\execution_graphs"
+
+def get_graph_features(g, source, target, name_initial):
+	features = dict()
+	features[name_initial + '_shortest_path_length'] = networkx.algorithms.shortest_path_length(g, source, target)
+	features[name_initial + '_num_paths'] = len(list(networkx.algorithms.simple_paths.all_simple_paths(g, source, target)))
+	features[name_initial + '_target_in_degree'] = g.in_degree(target)
+	features[name_initial + '_source_degree_centrality'] = networkx.algorithms.centrality.degree_centrality(g)[source]
+	features[name_initial + '_target_degree_centrality'] = networkx.algorithms.centrality.degree_centrality(g)[target]
+	features[name_initial + '_in_source_degree_centrality'] = networkx.algorithms.centrality.in_degree_centrality(g)[source]
+	features[name_initial + '_in_target_degree_centrality'] = networkx.algorithms.centrality.in_degree_centrality(g)[target]
+	features[name_initial + '_out_source_degree_centrality'] = networkx.algorithms.centrality.out_degree_centrality(g)[source]
+	features[name_initial + '_out_target_degree_centrality'] = networkx.algorithms.centrality.out_degree_centrality(g)[target]
+	return features
+
+
+instances = []
+with open(r"C:\amirelm\component_importnace\data\d4j_lang\tests_to_watch.json") as f:
+	tests_to_watch = json.loads(f.read())
+	
+for commit in tests_to_watch:
+	for commit_test in tests_to_watch[commit]:
+		for test, observation, component in itertools.product([commit_test[0]],[commit_test[1]],commit_test[2]):
+			features = {"observation" : observation}
+			for g_type in [call_graphs, execution_graphs]:
+				graph = networkx.read_gexf(os.path.join(g_type, commit, test+".gexf"))
+				test_node = filter(lambda node: test in node.lower(), graph.node)[0]
+				component_node = filter(lambda node: component in node.lower().replace("java.lang.", "").replace("java.io.", "").replace("java.util.", ""), graph.node)[0]
+				features.update(get_graph_features(graph, test_node, component_node, os.path.basename(g_type)))
+			instances.append(features)
+		
+
+with open(r"C:\amirelm\component_importnace\data\d4j_lang\training_graph.json", "wb") as f:
+	json.dump(instances, f)
+	
+keys = sorted(instances[0].keys())
+lines = map(lambda i: map(lambda k: i[k],keys), instances)
+with open(r"C:\amirelm\component_importnace\data\d4j_lang\training_graph.csv", "wb") as f:
+	csv.writer(f).writerows([keys] + lines)
+
---
+++ semantic_features.py
@@ -0,0 +1,39 @@
+import difflib
+import similarity
+import textdistance
+
+
+def count_type(diff, diff_type):
+	return len(filter(lambda d: d.startswith(diff_type), diff))
+
+def count_diff_changes(diff):
+	REMOVED = '- '
+	ADDED = '+ '
+	UNCHANGED = '  '
+	NOT_IN_INPUT = '? '
+	return count_type(diff, REMOVED), count_type(diff, ADDED), count_type(diff, UNCHANGED)
+
+def distance(test_name, component_name):
+	metrics = [textdistance.hamming, textdistance.mlipns, textdistance.levenshtein, textdistance.damerau_levenshtein, textdistance.jaro_winkler, textdistance.jaro, textdistance.strcmp95, textdistance.needleman_wunsch, textdistance.gotoh, textdistance.smith_waterman]
+	functions = ['distance', 'similarity', 'normalized_distance', 'normalized_similarity']
+	values = []
+	for metric in metrics:
+		for function in functions:
+			values.append(getattr(metric, function)(test_name, component_name))
+	return values
+
+def longets_substring(test_name, component_name):
+	seq_match = difflib.SequenceMatcher(None, test_name, component_name)
+	match = seq_match.find_longest_match(0, len(test_name), 0, len(component_name))
+	return match.size, seq_match.ratio()
+
+def semantic_names(test_name, component_name):
+	results = []
+	for test in [test_name, test_name.replace('test', '')]:
+		results.extend(count_diff_changes(difflib.ndiff(test, component_name)))
+		results.extend(longets_substring(test_name, component_name))
+		results.extend(distance(test_name, component_name))
+	return results
+	
+test_name, component_name = "testStandardInterval".lower(), "StandardInterval".lower()
+sn = semantic_names(test_name, component_name)
