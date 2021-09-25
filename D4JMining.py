207d4a53b219d5c0e6cfbdb09bc62982098476eb
amir9979 <amir9979@gmail.com>
2021-09-25 12:06:11 +0300

    Update D4JMining.py

--- D4JMining.py
+++ D4JMining.py
@@ -42,11 +42,8 @@ class D4JMining(D4JReproducer):
         super(D4JMining, self).__init__(id, fixed, failing_tests, dir_id)
         self.bug_mining_dir = bug_mining_dir
         self.matrix = os.path.join(self.bug_mining_dir, f"matrix_{id}.json")
-        self.init_data()
         self.opt_traces = []
-        with open(os.path.join(self.bug_mining_dir, 'test_details.json')) as f:
-            self.opt_traces = json.loads(f.read())
-        self.set_optimized_traces(self.get_optimized_traces())
+        self.optimized_traces = []
 
     def init_data(self):
         traces = list(JcovParser(None, [os.path.join(self.bug_mining_dir, 'result.xml')], True, True).parse(False))[
@@ -57,6 +54,9 @@ class D4JMining(D4JReproducer):
             tests_to_trace.append(TestResult(t, 'failure' if t in self.failing_tests else 'pass'))
         self.set_tests_to_trace(tests_to_trace)
         self.set_surefire_tests(dict(map(lambda x: (x.full_name, x), tests_to_trace)))
+        with open(os.path.join(self.bug_mining_dir, 'test_details.json')) as f:
+            self.opt_traces = json.loads(f.read())
+        self.set_optimized_traces(self.get_optimized_traces())
 
     def save_tests_results(self):
         data = dict(map(lambda x: (x[0], x[2] == 'pass'), self.opt_traces))
@@ -101,22 +101,28 @@ class D4JMining(D4JReproducer):
         with open(self.get_dir_id().traces_json, "w") as f:
             json.dump(traces, f)
 
+    def do_all(self):
+        self.init_data()
+        super(D4JMining, self).do_all()
+
     @staticmethod
     def get_all_projects(project_name, dir_path=DIR_BASE_PATH):
         projects = {}
-        bug_mining = os.path.join(D4JMining.D4J_DIR, project_name)
-        for dir_name in os.listdir(bug_mining):
+        df = None
+        base_bug_mining = os.path.join(D4JMining.D4J_DIR, project_name)
+        for dir_name in os.listdir(base_bug_mining):
             project_ind = int(dir_name.replace(D4JMining.D4J_PREFIX, ''))
-            bug_mining = os.path.join(bug_mining, dir_name, 'framework', 'projects')
+            bug_mining = os.path.join(base_bug_mining, dir_name, 'framework', 'projects')
             bug_mining = os.path.abspath(os.path.join(bug_mining, os.listdir(bug_mining)[0]))
             active_bugs = os.path.join(bug_mining, "active-bugs.csv")
             trigger_tests = os.path.join(bug_mining, "trigger_tests")
             path_to_trigger_tests = os.path.join(trigger_tests, os.listdir(trigger_tests)[0])
-            df = pd.read_csv(active_bugs)
+            if df is None:
+                df = pd.read_csv(active_bugs)
             bug_row = df[df['bug.id'] == project_ind]
             id, fixed = list(bug_row[["bug.id", "revision.id.fixed"]].iterrows())[0][1].to_list()
             failing_tests = []
-            with open(path_to_trigger_tests) as f:
+            with open(path_to_trigger_tests, errors='ignore') as f:
                 failing_tests = list(
                     map(lambda x: x[4:-1].replace('::', '.').lower(), filter(lambda l: l.startswith('---'), f.readlines())))
             projects[project_ind] = D4JMining(id, fixed, failing_tests, DirId(DirStructure(dir_path), str(id)), bug_mining)
@@ -135,9 +141,9 @@ class D4JMining(D4JReproducer):
 if __name__ == "__main__":
     dir_path = sys.argv[1]
     project_name = sys.argv[2]
-    if sys.argv[2] == 'base':
+    if dir_path == 'base':
         dir_path = DIR_BASE_PATH
-    try:
+    # try:
         if len(sys.argv) == 4:
             project = D4JMining.read_data_dir(sys.argv[3], project_name, dir_path).do_all()
         else: # 3
@@ -145,6 +151,6 @@ if __name__ == "__main__":
                 project.get_training_set()
                 ExperimentMatrix.experiment_classifiers(project.get_dir_id())
                 project.cleanup()
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+    #     print(e)
 
