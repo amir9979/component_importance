e83554cbd634e51c511530fc4c39cccb0b5d31ab
amir9979 <amir9979@gmail.com>
2021-09-26 17:55:29 +0300

    Update learning_classify.py

--- learning_classify.py
+++ learning_classify.py
@@ -108,7 +108,7 @@ class LearningClassify(InfluenceClassify):
         from sklearn.naive_bayes import GaussianNB
         from sklearn.ensemble import ExtraTreesClassifier
         from sklearn.feature_selection import VarianceThreshold
-        ans = [LearningClassify(dir_id, RandomForestClassifier(n_estimators=10, random_state=42))
+        ans = [LearningClassify(dir_id, RandomForestClassifier(n_estimators=10, random_state=42)),
                 LearningClassify(dir_id, GradientBoostingClassifier()),
                 LearningClassify(dir_id, DecisionTreeClassifier()),
                 LearningClassify(dir_id, MLPClassifier(solver='adam', alpha=1e-5, activation='relu', max_iter=3000, hidden_layer_sizes=(30, 30, 30, 30, 30), random_state=13)),
